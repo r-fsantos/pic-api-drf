fd480074e1525bc6fd31dae0518e03bca054289a
renato <felicio95.jr@gmail.com>
2023-12-18 19:11:16 -0300

    feat: creates Company model at app_company app

--- app_company/admin.py
+++ app_company/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_company.models import Company
+
+admin.site.register(Company)
--- app_company/models.py
+++ app_company/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    website = models.URLField(blank=True)
+    street_line_1 = models.CharField(max_length=255)
+    street_line_2 = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=80)
+    state = models.CharField(max_length=80)
+    zip_code = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
+
+    def __str__(self) -> str:
+        return self.name
